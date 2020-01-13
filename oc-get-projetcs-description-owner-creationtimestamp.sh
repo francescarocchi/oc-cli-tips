c8c907596bf922f20646a0ecb48c5ed676d5ec5d
Claudio Loletti <claudio.loletti@gmail.com>
2020-01-13 14:57:36 +0100

    correzione

--- oc-get-projetcs-description-owner-creationtimestamp.sh
+++ oc-get-projetcs-description-owner-creationtimestamp.sh
@@ -1,3 +1,3 @@
 # usando lo switch -k della sort è possibile cambiare il campo per il sorting
 
-IFS=,; while read data1 data2 data3 data4 ; do printf "%-60s ; %-70s ; %-50s ; %25s \n" "$data1 " "$data2 " "$data3" "$data4";  done < <(oc get projects -o template  --template '{{range .items}}{{.metadata.name}},{{index .metadata.annotations "openshift.io/description"}},{{index .metadata.annotations "openshift.io/requester"}},{{.metadata.name}}{{"\n"}}{{end }}' | sort -t, -k2 )
+IFS=,; while read data1 data2 data3 data4 ; do printf "%-60s ; %-70s ; %-50s ; %25s \n" "$data1 " "$data2 " "$data3" "$data4";  done < <(oc get projects -o template  --template '{{range .items}}{{.metadata.name}},{{index .metadata.annotations "openshift.io/description"}},{{index .metadata.annotations "openshift.io/requester"}},{{.metadata.creationdate}}{{"\n"}}{{end }}' | sort -t, -k2 )
