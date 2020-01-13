71aa74d356e7fc2597c4b51ec4e6f060eec89e16
lollonet <claudio.loletti@gmail.com>
2020-01-13 16:03:52 +0100

    changes

--- oc-get-projetcs-description-owner-creationtimestamp.sh
+++ oc-get-projetcs-description-owner-creationtimestamp.sh
@@ -1,3 +1,3 @@
 # usando lo switch -k della sort è possibile cambiare il campo per il sorting
 
-IFS=,; while read data1 data2 data3 data4 ; do printf "%-60s ; %-70s ; %-50s ; %25s \n" "$data1 " "$data2 " "$data3" "$data4";  done < <(oc get projects -o template  --template '{{range .items}}{{.metadata.name}},{{index .metadata.annotations "openshift.io/description"}},{{index .metadata.annotations "openshift.io/requester"}},{{.metadata.creationTimestamp}}{{"\n"}}{{end }}' | sort -t, -k2 )
+  while read data1 data2 data3 data4 ; do printf "%-60s ; %-70s ; %-50s ; %25s \n" "$data1 " "$data2 " "$data3 " "$data4 ";  done < <(oc get projects -o template  --template '{{range .items}}{{.metadata.name}}{{"\t"}}{{index .metadata.annotations "openshift.io/display-name"}}{{"\t"}}{{index .metadata.annotations "openshift.io/requester"}}{{"\t"}}{{.metadata.creationTimestamp}}{{"\n"}}{{end }}' | sort -t, -k2 )
--- oc-get-projetcs-description-owner.sh
+++ oc-get-projetcs-description-owner.sh
@@ -1,3 +1,3 @@
 # usando lo switch -k della sort è possibile cambiare il campo per il sorting
 
-IFS=,; while read data1 data2 data3 ; do printf "%-60s ; %-70s ; %-50s \n" "$data1 " "$data2 " "$data3";  done < <(oc get projects -o template  --template '{{range .items}}{{.metadata.name}},{{index .metadata.annotations "openshift.io/description"}},{{index .metadata.annotations "openshift.io/requester"}}{{"\n"}}{{end }}' | sort -t, -k2 )
+IFS=,; while read data1 data2 data3 ; do printf "%-60s ; %-70s ; %-50s \n" "$data1 " "$data2 " "$data3";  done < <(oc get projects -o template  --template '{{range .items}}{{.metadata.name}},{{index .metadata.annotations "openshift.io/display-name"}},{{index .metadata.annotations "openshift.io/requester"}}{{"\n"}}{{end }}' | sort -t, -k2 )
